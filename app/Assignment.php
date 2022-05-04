c3098cfc16522e07b8500937da29e58f9088c811
sparsh_36 <66721570+intelligently-artificial@users.noreply.github.com>
2022-05-04 14:56:15 +0530

    Add files via upload

---
+++ app/Assignment.php
@@ -0,0 +1,30 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Assignment extends Model
+{
+    protected $table="assignment";
+    protected $primaryKey="assignment_id";
+
+    public static function assign($name , $assignment , $course){
+        if(!empty($name) && (!empty($assignment)) && (!empty($course))){
+            $assign = new Assignment;
+
+            $assign->name       = $name;
+            $assign->course     = $course;
+            $assign->assignment = $assignment;
+
+            return $assign->save();
+        }
+    }
+
+    public static function assignmentDetails(){
+        $assignment = Self::all();
+        return $assignment;
+    }
+
+    public static function pagination(){
+        $assignment = Self::paginate(10);
+        return $assignment;
+    }
+}
---
+++ app/Student.php
@@ -0,0 +1,98 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+use App\Assignment;
+
+class Student extends Model
+{
+    protected $table="student";
+    protected $primaryKey="student_id";
+
+    public static function register($name , $course , $email , $password , $number , $gender){
+        if((!empty($name)) && (!empty($email)) && (!empty($course)) && (!empty($password)) && (!empty($number)) && (!empty($gender)) && (!empty($experience))){
+
+
+            $register = new Student;
+    
+            $register->name     = $name;
+            $register->course   = $course;
+            $register->email    = $email;
+            $register->password = $password;
+            $register->number   = $number;
+            $register->gender   = $gender;
+    
+            return $register->save();
+        }
+    }
+
+    public static function checkLogin($email){
+        if(!empty($email)){
+            $check=Self::where('email',$email)->get();
+            return $check; 
+        }       
+    }
+
+    public static function login($email,$password){
+        if(!empty($email) && (!empty($password))){
+            $login=Self::where(['email'=>$email,'password'=>$password])->get();
+            return $login;   
+        }    
+    }
+
+    public static function studentView($search){
+        if(!empty($search)){
+            $student=Self::where('name','LIKE',"%$search%")->orwhere('email','LIKE',"%$search%")->paginate(2);
+            return $student;
+        }
+    }
+
+    public static function deleteStudent($id){
+        if(!empty($id)){
+            $data = Self::find($id)->delete();
+            return $data;
+        }
+    }
+
+    public static function edit($id){
+        if(!empty($id)){
+            $student = Self::find($id);
+            return $student;
+        }
+    }
+    
+
+    public static function updateStudent($id , $name , $email , $course , $number , $gender){
+        if((!empty($name)) && (!empty($email)) && (!empty($course)) && (!empty($id)) && (!empty($number)) && (!empty($gender))){
+
+            $register = Self::find($id);
+
+            $register->name   = $name;
+            $register->email  = $email;
+            $register->course = $course;
+            $register->number = $number;
+            $register->gender = $gender;
+
+            return $register->save();
+        }
+
+    }
+
+    public static function forApi(){
+        $data=Self::select('student.email', 'teacher.name', 'assignment.assignment','student.course')
+        ->join('assignment', 'student.course', '=', 'assignment.course')
+        ->join('teacher', 'teacher.course', '=', 'assignment.course')
+        ->get();
+        return $data;
+    }
+
+    public static function studentDetails(){
+        $student = Self::all();
+        return $student;
+    }
+
+    public static function pagination(){
+        $student = Self::paginate(2);
+        return $student;
+    }
+
+}
