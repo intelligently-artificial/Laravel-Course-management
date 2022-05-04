3633cdb9e4f0b061557ade7374d5859f2e214dc4
sparsh_36 <66721570+intelligently-artificial@users.noreply.github.com>
2022-05-04 15:00:21 +0530

    Add files via upload

---
+++ app/Http/Requests/TeacherRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TeacherRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */ 
+    public function rules()
+    {
+        return [
+            
+                'name' => 'required|string|max:255',
+                'course' => 'required|string',
+                'experience' => 'required|integer',
+                'gender' => 'required', 
+                'number' => 'required',
+                'email' => 'required|email|unique:Teacher',
+                'password' => 'required|min:8|max:50'
+
+              ];
+    }
+}
