16e690fbd9fec6d4877d778d77bce2e720e577e3
sparsh_36 <66721570+intelligently-artificial@users.noreply.github.com>
2022-05-04 12:44:56 +0530

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -3,39 +3,39 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/register','StudentController@create');
 Route::post('/register','StudentController@store');
-Route::get('/login','StudentController@loginview');
+Route::get('/login','StudentController@loginView');
 Route::post('/login','StudentController@login');
-Route::get('/home','StudentController@homepage');
+Route::get('/home','StudentController@homePage');
 
-Route::get('/tregister','TeacherController@create');  
-Route::post('/tregister','TeacherController@store');
-Route::get('/tlogin','TeacherController@loginview');
+Route::get('/teacher/register','TeacherController@create');  
+Route::post('/teacher/register','TeacherController@store');
+Route::get('/teacher/login','TeacherController@loginView');
 Route::post('/teacher/view','TeacherController@login');
 
 Route::group(['middleware'=>'check'],function()
 {
-Route::delete('/student/delete/{student_id}','StudentController@destroy')->name('student.delete');
-Route::put('/student/edit/{student_id}','StudentController@edit')->name('student.edit');
-Route::post('/student/update/{student_id}','StudentController@update')->name('student.update');
-Route::get('/link','TeacherController@login');
-Route::get('/teacherpersonal-view','TeacherController@show');
-Route::get('/students','TeacherController@mystudentsview');
-Route::get('/teacher/view','TeacherController@login');
-Route::delete('/teacher/delete/{teacher_id}','TeacherController@destroy')->name('teacher.delete');
-Route::put('/teacher/edit/{teacher_id}','TeacherController@edit')->name('teacher.edit');
-Route::post('/teacher/update/{teacher_id}','TeacherController@update')->name('teacher.update');
-Route::get('/assignment','AssignmentController@index');
-Route::post('/assignment','AssignmentController@assign');
-Route::get('/viewassignment','AssignmentController@view');
-Route::get('/adminassignment','AssignmentController@adminview');
-Route::get('/teacheradminview','AdminController@teacheradminview') -> name('teacher');
-Route::get('/student/view','StudentController@studentview') -> name('student');
+    Route::delete('/student/gone/{student_id}','StudentController@destroy')->name('student.delete');
+    Route::get('/student/do/{student_id}','StudentController@edit')->name('student.edit');
+    Route::put('/student/update/{student_id}','StudentController@update')->name('student.update');
+    Route::get('/link','TeacherController@login');
+    Route::get('/teachers/view','TeacherController@show');
+    Route::get('/students','TeacherController@myStudentsView');
+    Route::get('/teacher/view','TeacherController@login');
+    Route::delete('/teacher/gone/{teacher_id}','TeacherController@destroy')->name('teacher.delete');
+    Route::get('/teacher/do/{teacher_id}','TeacherController@edit')->name('teacher.edit');
+    Route::put('/teacher/update/{teacher_id}','TeacherController@update')->name('teacher.update');
+    Route::get('/assignment','AssignmentController@index');
+    Route::post('/assignment','AssignmentController@assign');
+    Route::get('/view/assignment','AssignmentController@view');
+    Route::get('/admin/assignment','AssignmentController@adminView');
+    Route::get('/teacher/admin/view','AdminController@teacherAdminView')->name('teacher');
+    Route::get('/student/view','StudentController@studentView')->name('student');
 });
 
 Route::get('/logout','StudentController@logout');
-Route::get('/alogin','AdminController@loginview');
-Route::post('/alogin','AdminController@login');
-Route::get('/helloadmin','AdminController@helloadmin')-> middleware('check');
+Route::get('/admin/login','AdminController@loginView');
+Route::post('/admin/login','AdminController@login');
+Route::get('/hello/admin','AdminController@helloAdmin')->middleware('check');
 
 
 
